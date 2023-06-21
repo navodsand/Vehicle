e5d5f2ba8605117a632111f421b426995a02c61d
navodsand <sandaruwannavod223@gmail.com>
2023-06-21 17:46:55 +0530

    vehicle

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+ 
+ function display(){
+
+    const vehicleInfo = `<h1>Details</h1>
+    <img src="land .jpg" alt="Car Image" />
+    <table>
+      <tr>
+        <th>Brand</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Model</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Price</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Year</th>
+        <td></td>
+      </tr>
+      <tr>
+        <th>Color</th>
+        <td></td>
+      </tr>
+    </table>`;
+ }
+ 
+ 
  const xhr = new XMLHttpRequest();
 
  xhr.onreadystatechange = function() {
